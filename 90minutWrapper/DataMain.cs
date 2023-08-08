ab7ea13e81cec9424937b8e2b3746c5a5e767ef3
shiklax <radek32411@gmail.com>
2023-08-08 10:53:44 +0200

    Add project files.

---
+++ 90minutWrapper/DataMain.cs
@@ -0,0 +1,54 @@
+﻿using HtmlAgilityPack;
+using System.Net;
+using System.Text;
+
+namespace _90minutWrapper
+{
+    public class DataMain
+    {
+        public static List<string> GetData(int clubNumber = 8711)
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            var iso88592 = Encoding.GetEncoding(28592);
+            var url = $"http://www.90minut.pl/skarb.php?id_klub={clubNumber}";
+            var web = new HtmlWeb();
+            web.OverrideEncoding = iso88592;
+            var doc = web.Load(url);
+            //var doc = new HtmlDocument();
+            //var filePath = @"C:\Users\Radek\Downloads\Skarb - Kolektyw Radwanice.html";
+            //doc.Load(filePath, iso88592);
+
+            var nodes = doc.DocumentNode.SelectNodes("//tr");
+            var count = 0;
+            var strList = new List<string>();
+            if (nodes != null)
+            {
+                foreach (var trNode in nodes)
+                {
+
+                    var tdNodes = trNode.SelectNodes("./td"); // Select td elements within each tr element
+
+                    if (tdNodes != null)
+                    {
+                        foreach (var tdNode in tdNodes)
+                        {
+                            var result = WebUtility.HtmlDecode(tdNode.InnerText.Trim());
+                            if (result.Length == 0 || count < 16)
+                            {
+                                count++;
+                                continue;
+                            }
+                            strList.Add(result);
+                            count++;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                throw new Exception("Nothing was found!.");
+            }
+            return strList;
+        }
+    }
+}
---
+++ Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Helper
+{
+    public List<string> ExtractToList(string input, string[] delimiter, bool replaceWhiteSpace)
+    {
+        var temp = ExtractColons(input);
+        if (replaceWhiteSpace)
+        {
+            temp = ExtractColons(input.Replace(" ", ""));
+        }
+        List<string> result = new List<string>();
+        string[] lines = temp.Split(delimiter, StringSplitOptions.RemoveEmptyEntries);
+
+        foreach (var line in lines)
+        {
+            result.Add(line);
+        }
+        return result;
+    }
+    public List<string> ExtractToList(List<string> list)
+    {
+        var result = new List<string>();
+        foreach (var item in list)
+        {
+            var temp = item.Replace("&nbsp;", "");
+            result.Add(temp);
+        }
+        return result;
+    }
+    public string ExtractColons(string input)
+    {
+        int firstColonIndex = input.IndexOf(':');
+        if (firstColonIndex >= 0)
+        {
+            string ExtractColonsedDate = input.Substring(firstColonIndex + 1).Trim();
+            return ExtractColonsedDate;
+        }
+        return input;
+    }
+}
